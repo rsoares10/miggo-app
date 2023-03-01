250231bb621e8e464294512115117f6fb477857a
rsoares10 <rsoares.monteiro10@gmail.com>
2023-03-01 01:44:47 -0300

    style: add light theme scaffold background color

--- lib/themes/light.theme.dart
+++ lib/themes/light.theme.dart
@@ -3,5 +3,6 @@ import 'package:flutter/material.dart';
 ThemeData lightTheme() {
   return ThemeData(
     primarySwatch: Colors.yellow,
+    scaffoldBackgroundColor: const Color(0xfff7f7f7),
   );
 }

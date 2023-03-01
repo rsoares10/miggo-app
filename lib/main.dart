9893878ba546cff222635ef8b074ffc4d9e250b0
rsoares10 <rsoares.monteiro10@gmail.com>
2023-03-01 01:42:48 -0300

    style: remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Miggo',
       theme: lightTheme(),
+      debugShowCheckedModeBanner: false,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }

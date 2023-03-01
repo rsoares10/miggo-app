abf66b64979dd95a69a67aa6c160432d85de3f60
rsoares10 <rsoares.monteiro10@gmail.com>
2023-03-01 01:58:57 -0300

    feature: add onboarding view

---
+++ lib/views/onboarding.view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Onboardingview extends StatefulWidget {
+  const Onboardingview({super.key});
+
+  @override
+  State<Onboardingview> createState() => _OnboardingviewState();
+}
+
+class _OnboardingviewState extends State<Onboardingview> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
